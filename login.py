56cfd7d8cdafbfb41fb0b64ee99b3a6bab50278f
sandeep <sandeepvardhan9381@gmail.com>
2026-03-24 17:25:35 +0530

    added explicit wait and some error handling

--- login.py
+++ login.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver=webdriver.Chrome()
-driver.get("https://the-internet.herokuapp.com/login")
-driver.find_element(By.CSS_SELECTOR,"#username").send_keys("tomsmith")
-driver.find_element(By.CSS_SELECTOR,"#password").send_keys("SuperSecretPassword!")
-driver.find_element(By.CSS_SELECTOR,".radius").click()
-time.sleep(1)
-print(driver.title)
+driver=webdriver.Chrome()          #driver opens url in chrome
+driver.maximize_window()           # to maximize thw window of website
 try:
-    message=driver.find_element(By.CSS_SELECTOR,".flash.success").text
-    if "You logged into a secure area!" in message:
+    driver.get("https://the-internet.herokuapp.com/login")  #website url
+    wait=WebDriverWait(driver,10)  #driver waits 10 seconds , but if the element appears it runs code
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#username")))           #finding element
+    driver.find_element(By.CSS_SELECTOR,"#username").send_keys("tomsmith")              # finding element + action
+    driver.find_element(By.CSS_SELECTOR,"#password").send_keys("SuperSecretPassword!")  # finding element + action
+    driver.find_element(By.CSS_SELECTOR,".radius").click()                              # finding element + action
+    message=wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".flash.success")))
+    m=message.text        #checking if logged into website or not
+    if "You logged into a secure area!" in m:
         print("pass")
     else:
         print("fail")
 except:
-    print("element not found")
-    
-time.sleep(2)
-driver.quit()
+    print("element not found")                  # if element not found it prints "element not found"
+finally:
+    driver.quit()                  #web driver quits after completing the task
