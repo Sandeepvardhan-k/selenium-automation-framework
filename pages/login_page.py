68c5d17081c1b56c19338e36bb4f7575d637f68c
sandeep <sandeepvardhan9381@gmail.com>
2026-03-27 19:42:40 +0530

    add docker for selenium automation checker

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pytest
 class LoginPage:
     def __init__(self,driver):
         self.driver = driver
